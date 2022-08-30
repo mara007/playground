406fd95376e31259469f5b746604e14484dad323
Marek <marek@cz-wintermute.cz>
2022-08-30 10:26:38 +0200

    thekal discord data

--- ascii_chart.py
+++ ascii_chart.py
@@ -7,8 +7,8 @@ class Chart:
     def __init__(self, data: dict) -> None:
         self.data = data
         self.max_bar_chars = 150
-        self.bar_chars = '🧙🦄🦴👾🕷🌻🌶'
         self.bar_chars = '🧙🦄🦴👾🌻'
+        self.bar_chars = '🧙🦄🦴👾🕷🌻🌶'
         # self.bar_chars = '🍓'
         self.bar_char_last = 0
         self.padding = 4
@@ -47,22 +47,38 @@ class Chart:
             bar = self.__get_percent_bar(value, sum_of_all)
             print(f'{key:>{longest_key + self.padding}}{" ":>{self.padding}}{percent:>{6}}{" ":>{self.padding}}{bar:<{longest_key + 2* self.padding}}')
 
+        print(f'{"votes":>{longest_key + self.padding}}{" ":>{self.padding}}{sum_of_all:>{6}}')
+
 
 if __name__ == '__main__':
-    # 12.8.2022
     data = {
-        'wotlk_fresh_disc': {
+        # 30.8.2022
+        'wotlk_fresh_thekal': {
             'druid': 155,
-            'death knight': 184,
-            'paladin': 239,
-            'mage': 104,
-            'hunter': 110,
-            'shaman': 123,
-            'priest': 114,
-            'rogue': 117,
-            'warrior': 108,
-            'warlock': 104
+            'death knight': 143,
+            'paladin': 246,
+            'mage': 103,
+            'hunter': 111,
+            'shaman': 101,
+            'priest': 104,
+            'rogue': 131,
+            'warrior': 130,
+            'warlock': 108
         },
+        # 22.8.2022
+        'wotlk_fresh_disc': {
+            'druid': 197,
+            'death knight': 220,
+            'paladin': 291,
+            'mage': 129,
+            'hunter': 134,
+            'shaman': 152,
+            'priest': 152,
+            'rogue': 144,
+            'warrior': 147,
+            'warlock': 131
+        },
+        # 12.8.2022
         'wotlk_disc': {
             'druid': 270,
             'death knight': 500,
