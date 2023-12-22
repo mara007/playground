c9d1b46d4c659141f80cc7598a6eea6eca9a269b
Marek <marek@Wintermute2>
2023-12-22 09:01:04 +0100

    tester_utf

--- tester.cpp
+++ tester.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
 #include <thread>
 
+struct tester_t {
+    tester_t() { std::cout << "tester_t()\n\n"; }
+    tester_t(const tester_t& t) { std::cout << "tester_t(tester_t&)\n\n"; }
+    tester_t& operator=(const tester_t& rhs) {
+        std::cout << "tester_t::operator=()\n\n";
+        return *this;
+    }
+    tester_t& operator=(const tester_t&& rhs) {
+        std::cout << "tester_t::operator=(move)\n\n";
+        return *this;
+    }
+     tester_t(tester_t&& t) { std::cout << "tester_t(move)\n\n"; }
+    virtual ~tester_t() { std::cout << "~tester_t\n\n"; }
+};
+
 int main (int argc, char * argv [])
 {
     std::cout << "Hello world! of git" << std::endl;
 
+    std::cout << "constr\n";
+    tester_t t1;
+    std::cout << "copy oper\n";
+    tester_t t2 = t1;
+    std::cout << "copy const\n";
+    tester_t t3(t1);
+    std::cout << "copy oper2\n";
+    t1 = t2;
+    std::cout << "copy constr from tmp\n";
+    tester_t t4{tester_t()};
+    std::cout << "copy oper from tmp\n";
+    tester_t t5 = tester_t();
+    std::cout << "move\n";
+    t4 = std::move(t5);
+    std::cout << "constr move\n";
+    tester_t t6(std::move(t5));
+
+
+  
     int i = 0;
     do {
         std::cout << "i=" << i << std::endl;
---
+++ tester_utf.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <locale>
+
+int main() {
+    std::cout << "Hello, world!\n";
+    return 0;
+}
